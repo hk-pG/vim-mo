9c56b615b9cdcd0336ff4f0b1ee64a3310e38beb
hk-pG <hukada.gogotea@gmail.com>
2026-03-08 01:56:25 +0900

    fix(nvim): vimmo-ls が起動しない問題を修正（filetype検出 + astrolsp登録）

--- docker/lua/plugins/vimmo-ls.lua
+++ docker/lua/plugins/vimmo-ls.lua
@@ -1,11 +1,13 @@
 return {
-  "neovim/nvim-lspconfig",
+  "AstroNvim/astrolsp",
   opts = {
-    servers = {
+    -- vimmo_ls は Mason 管理外のため手動で有効化
+    servers = { "vimmo_ls" },
+    config = {
       vimmo_ls = {
         cmd = { "vimmo-ls" },
         filetypes = { "vmo" },
-        root_dir = function(fname)
+        root_dir = function()
           return vim.loop.cwd()
         end,
       },
